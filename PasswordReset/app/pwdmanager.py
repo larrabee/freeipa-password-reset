15e34e2ab525542dffda71bfa4b5877b7a8e1b84
vladimir <kafatron@gmail.com>
2017-10-16 09:13:09 +0200

    Fix type error in password change funcion

--- PasswordReset/app/pwdmanager.py
+++ PasswordReset/app/pwdmanager.py
@@ -53,7 +53,7 @@ class PasswdManager():
     
     def __set_password(self, uid, password):
         try:
-            password_exp_days = api.Command.pwpolicy_show()['result']['krbmaxpwdlife'][0]
+            password_exp_days = int(api.Command.pwpolicy_show()['result']['krbmaxpwdlife'][0])
             date = (datetime.now() + timedelta(days=password_exp_days)).strftime("%Y%m%d%H%M%SZ")
             api.Command.user_mod(uid=unicode(uid), userpassword=unicode(password))
             api.Command.user_mod(uid=unicode(uid), setattr=unicode("krbPasswordExpiration={0}".format(date)))
