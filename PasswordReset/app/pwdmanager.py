7957bd8844344839f84a69db82b7b0c1a9432f3f
Vladimir Buyanov <kafatron@gmail.com>
2019-12-11 11:41:37 +0200

    Reset count of 'krbloginfailedcount' on password change event

--- PasswordReset/app/pwdmanager.py
+++ PasswordReset/app/pwdmanager.py
@@ -61,6 +61,7 @@ class PasswdManager():
             date = (datetime.now() + timedelta(days=password_exp_days)).strftime("%Y%m%d%H%M%SZ")
             api.Command.user_mod(uid=unicode(uid), userpassword=unicode(password))
             api.Command.user_mod(uid=unicode(uid), setattr=unicode("krbPasswordExpiration={0}".format(date)))
+            api.Command.user_mod(uid=unicode(uid), setattr=unicode("krbloginfailedcount=0"))
         except Exception as e:
             raise SetPasswordFailed("Cannot update your password. {0}".format(e))
                     
